e336d79777f3338eb0e3100edecfa14b5a937bbf
altairnative <altemia.decoy@gmail.com>
2018-03-18 02:38:12 +0800

    Setup ember pod structure

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'transportasyon-fe',
+    podModulePrefix: 'transportasyon-fe/pods',
     environment,
     rootURL: '/',
     locationType: 'auto',
